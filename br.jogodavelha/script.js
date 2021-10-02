b127f511c1d02d43ccc8f59ee4d6a3f957f8615d
Carlos <carloseduardo2101@gmail.com>
2021-10-02 14:12:09 -0300

    Update Jogo da Velha

---
+++ br.jogodavelha/script.js
@@ -0,0 +1,25 @@
+// 0 = circulo, 1 = xis, -1 = vazio
+
+var tabela = new Array(3);
+
+tabela[0] = [-1, -1, -1];
+tabela[1] = [-1, -1, -1];
+tabela[2] = [-1, -1, -1];
+
+var vez = 0;
+
+function jogada(vez, celula){
+    
+    /*
+    * Caso vez seja do circulo
+    *   
+    *   1)  
+    * */
+    
+    switch (vez){
+        case 0:
+            break;
+        case 1:
+            break;
+    }
+}
